139494aff717393b0f87fbcf453ceb3867e24c69
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-08-15 21:30:56 +0300

    initial project

--- src/App.js
+++ src/App.js
@@ -1,60 +1,89 @@
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
+    {
+        title: "Ибраим - это. . .",
+        variants: ["токсик", "супер-токсик", "ультра-токсик"],
+        correct: 2,
+    },
+    {
+        title: "Любимое аниме Эрнеса",
+        variants: ["наруто", "жо жо", "боруто"],
+        correct: 2,
+    },
+    {
+        title: "Какая игра на канале Помидорчик плей набрала больше всего просмотров?",
+        variants: ["стрей", "фортнайт", "майнкрафт"],
+        correct: 3,
+    },
 ];
 
-function Result() {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
+function Result({ correct }) {
+    return (
+        <div className="result">
+            <img
+                src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
+                alt="result-img"
+            />
+            <h2>
+                Вы отгадали {correct} ответа из {questions.length}
+            </h2>
+            <a href="/">
+                <button>Попробовать снова</button>
+            </a>
+        </div>
+    );
 }
 
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
-      </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
-      </ul>
-    </>
-  );
+function Game({ step, question, handleAnswer }) {
+    const percent = Math.round((step / questions.length) * 100);
+
+    return (
+        <>
+            <div className="progress">
+                <div
+                    style={{ width: `${percent}%` }}
+                    className="progress__inner"
+                ></div>
+            </div>
+            <h1>{question.title}</h1>
+            <ul>
+                {question.variants.map((quiz, i) => (
+                    <li onClick={() => handleAnswer(i)} key={quiz}>
+                        {quiz}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Game />
-      {/* <Result /> */}
-    </div>
-  );
+    const [step, setStep] = useState(0);
+    const [correct, setCorrect] = useState(0);
+    const question = questions[step];
+
+    const handleAnswer = (i) => {
+        if (i === question.correct) {
+            setCorrect(correct + 1);
+        }
+        setStep(step + 1);
+    };
+
+    return (
+        <div className="App">
+            {step !== questions.length ? (
+                <Game
+                    step={step}
+                    question={question}
+                    handleAnswer={handleAnswer}
+                />
+            ) : (
+                <Result correct={correct} />
+            )}
+        </div>
+    );
 }
 
 export default App;
